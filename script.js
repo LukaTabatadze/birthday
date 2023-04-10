bffee3a572a6211c0e414f494d1ed86dc313584c
LukaTabatadze <ltaluka2003@gmail.com>
2023-04-10 23:54:35 +0400

    sdfgh

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-document.getElementById("start-btn").addEventListener("click", function() {
-    document.getElementById("start-btn").style.display = "none";
-    document.getElementById("birthday-site").style.display = "block";
-    document.getElementById("birthday-song").style.display = "block";
-    document.getElementById("birthday-song").play();
-  });
 
   $(document).ready(function() {
     $("#start-btn").click(function() {
-      $("#start-btn").hide();
-      $("#birthday-site").removeClass("d-none");
-      startConfetti(); // Add this line to start the confetti animation
+      $("#start-btn").fadeOut(); // Fade out the button
+      $("#birthday-site").fadeIn(); // Fade in the content
+      document.getElementById("birthday-song").play(); // Play the song
     });
   });
